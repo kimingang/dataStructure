981372d6afc5b174b3093b08e803b63129cf31e0
kimingang <sofargang@gmail.com>
2022-05-13 17:10:58 +0900

    fixing

---
+++ 10_Graph/01_AdjacentListGraph/ALGraph.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ALGraph.h"
+#include "DLinkedList.h"
+
+int WhoIsPrecede(int data1, int data2);
+
+// 그래프의 초기화
+void GraphInit(ALGraph * pg, int nv)
+{
+	int i;	
+
+	pg->adjList = (List*)malloc(sizeof(List)*nv);
+	pg->numV = nv;
+	pg->numE = 0;     // 초기의 간선 수는 0개
+
+	for(i=0; i<nv; i++)
+	{
+		ListInit(&(pg->adjList[i]));
+		SetSortRule(&(pg->adjList[i]), WhoIsPrecede); 
+	}
+}
+
+// 그래프 리소스의 해제
+void GraphDestroy(ALGraph * pg)
+{
+	if(pg->adjList != NULL)
+		free(pg->adjList);
+}
+
+// 간선의 추가
+void AddEdge(ALGraph * pg, int fromV, int toV)
+{
+	LInsert(&(pg->adjList[fromV]), toV);
+	LInsert(&(pg->adjList[toV]), fromV);
+	pg->numE += 1;
+}
+
+// 유틸리티 함수: 간선의 정보 출력
+void ShowGraphEdgeInfo(ALGraph * pg)
+{
+	int i;
+	int vx;
+
+	for(i=0; i<pg->numV; i++)
+	{
+		printf("%c와 연결된 정점: ", i + 65);
+		
+		if(LFirst(&(pg->adjList[i]), &vx))
+		{
+			printf("%c ", vx + 65);
+			
+			while(LNext(&(pg->adjList[i]), &vx))
+				printf("%c ", vx + 65);
+		}
+		printf("\n");
+	}
+}
+
+int WhoIsPrecede(int data1, int data2)
+{
+	if(data1 < data2)
+		return 0;
+	else
+		return 1;
+}
+
+/*
+#include <stdio.h>
+#include "ALGraph.h"
+
+int main(void)
+{
+	ALGraph graph;
+	GraphInit(&graph, 5);     // A, B, C, D, E의 정점 생성
+
+	AddEdge(&graph, A, B);
+	AddEdge(&graph, A, D);
+	AddEdge(&graph, B, C);
+	AddEdge(&graph, C, D);
+	AddEdge(&graph, D, E);
+	AddEdge(&graph, E, A);
+
+	ShowGraphEdgeInfo(&graph);
+
+	GraphDestroy(&graph);
+	return 0;
+}
+*/
